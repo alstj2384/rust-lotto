fa7282f6f6a00fde61e4ea8565a8315dc008a0b7
alstj2384 <ass91160@naver.com>
2025-11-09 14:20:58 +0900

    feat(main): 당첨번호 및 보너스 번호 입력 기능 구현

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, vec};
 
 fn main() {
     // 구매할 로또 금액 입력받기
@@ -7,17 +7,49 @@ fn main() {
         .read_line(&mut money)
         .expect("구매 금액이 잘못되었습니다!");
 
-    money = money
+    let money: i32 = money
         .trim()
         .parse()
         .expect("구매 금액은 숫자만 입력할 수 있습니다!");
 
-    println!("{}", money);
     // 생성된 로또 개수 보여주기
     // 생성된 로또 보여주기
 
     // 당첨 로또 번호 입력받기
+    let mut wining_lotto_numbers = String::new();
+    io::stdin()
+        .read_line(&mut wining_lotto_numbers)
+        .expect("입력이 잘못되었습니다.");
+
+    let lotto_numbers = wining_lotto_numbers.split(",");
+
+    let mut lotto_numbers_vec: Vec<i32> = Vec::new();
+
+    for number in lotto_numbers {
+        lotto_numbers_vec.push(
+            number
+                .trim()
+                .parse()
+                .expect("로또 번호는 숫자만 입력해야 합니다."),
+        );
+    }
+
+    for number in lotto_numbers_vec {
+        println!("{}", number);
+    }
+
     // 보너스 번호 입력받기
+    let mut bonus_number = String::new();
+    io::stdin()
+        .read_line(&mut bonus_number)
+        .expect("입력이 잘못되었습니다.");
+
+    let bonus_number: i32 = bonus_number
+        .trim()
+        .parse()
+        .expect("보너스 번호는 숫자만 입력해야 합니다.");
+
+    println!("{}", bonus_number);
 
     // 결과를 출력하기
     // 수익률 출력하기
