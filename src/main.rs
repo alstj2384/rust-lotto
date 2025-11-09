e8248e0dc287bde01e1a30551d51aa1fcb43a539
alstj2384 <ass91160@naver.com>
2025-11-09 16:16:14 +0900

    feat(main): 로또 당첨 로직 구현 및 통계 출력 구현

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::{io, vec};
+use std::io;
 
 fn main() {
     // 구매할 로또 금액 입력받기
@@ -49,6 +49,7 @@ fn main() {
     println!();
 
     // 당첨 로또 번호 입력받기
+    println!("당첨 번호를 입력해 주세요.");
     let mut wining_lotto_numbers = String::new();
     io::stdin()
         .read_line(&mut wining_lotto_numbers)
@@ -66,12 +67,10 @@ fn main() {
                 .expect("로또 번호는 숫자만 입력해야 합니다."),
         );
     }
-
-    for number in lotto_numbers_vec {
-        println!("{}", number);
-    }
+    println!("");
 
     // 보너스 번호 입력받기
+    println!("보너스 번호를 입력해 주세요.");
     let mut bonus_number = String::new();
     io::stdin()
         .read_line(&mut bonus_number)
@@ -81,9 +80,93 @@ fn main() {
         .trim()
         .parse()
         .expect("보너스 번호는 숫자만 입력해야 합니다.");
-
-    println!("{}", bonus_number);
+    println!();
 
     // 결과를 출력하기
+    let mut sum: u64 = 0;
+    let mut result: Vec<i32> = Vec::new();
+    for _ in 0..6 {
+        result.push(0);
+    }
+
+    // 1. 로또 번호들과 기본 번호 + 당첨 번호를 비교하기
+    for lotto in lottos {
+        let mut count = 0;
+        let mut is_bonus_correct = false;
+        for number in &lotto_numbers_vec {
+            if lotto.contains(&number) {
+                count += 1;
+            }
+        }
+        if lotto.contains(&bonus_number) {
+            is_bonus_correct = true;
+        }
+
+        if count == 6 {
+            sum += 2_000_000_000;
+            if let Some(num) = result.get_mut(0) {
+                *num += 1;
+            }
+        } else if count == 5 && is_bonus_correct {
+            sum += 30_000_000;
+            if let Some(num) = result.get_mut(1) {
+                *num += 1;
+            }
+        } else if count == 5 {
+            sum += 1_500_000;
+            if let Some(num) = result.get_mut(2) {
+                *num += 1;
+            }
+        } else if count == 4 {
+            sum += 50_000;
+            if let Some(num) = result.get_mut(3) {
+                *num += 1;
+            }
+        } else if count == 3 {
+            sum += 5_000;
+            if let Some(num) = result.get_mut(4) {
+                *num += 1;
+            }
+        } else {
+            if let Some(num) = result.get_mut(5) {
+                *num += 1;
+            }
+        }
+    }
+
+    println!("당첨 통계");
+    println!("---");
+    // 2. 당첨 등수를 산정하고 결과 Map에 저장하기, 총 수익률에 더하기
+    println!(
+        "{}개 일치 ({}원) - {}개",
+        3,
+        "5,000",
+        result.get(4).unwrap()
+    );
+    println!(
+        "{}개 일치 ({}원) - {}개",
+        4,
+        "50,000",
+        result.get(3).unwrap()
+    );
+    println!(
+        "{}개 일치 ({}원) - {}개",
+        5,
+        "1,500,000",
+        result.get(2).unwrap()
+    );
+    println!(
+        "{}개 일치, 보너스 볼 일치 ({}원) - {}개",
+        5,
+        "30,000,000",
+        result.get(1).unwrap()
+    );
+    println!(
+        "{}개 일치 ({}원) - {}개",
+        6,
+        "2,000,000,000",
+        result.get(0).unwrap()
+    );
+
     // 수익률 출력하기
 }
