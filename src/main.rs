7bdcbb9a776373ae39311fc1f90d5cf9d20aa3be
alstj2384 <ass91160@naver.com>
2025-11-09 13:22:47 +0900

    feat(main): 로또 구입 금액 입력 기능 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // 구매할 로또 금액 입력받기
+    let mut money = String::new();
+    io::stdin()
+        .read_line(&mut money)
+        .expect("구매 금액이 잘못되었습니다!");
+
+    money = money
+        .trim()
+        .parse()
+        .expect("구매 금액은 숫자만 입력할 수 있습니다!");
+
+    println!("{}", money);
+    // 생성된 로또 개수 보여주기
+    // 생성된 로또 보여주기
+
+    // 당첨 로또 번호 입력받기
+    // 보너스 번호 입력받기
+
+    // 결과를 출력하기
+    // 수익률 출력하기
 }
