b82324082584bbb91fe3ad7e960829b0788de004
alstj2384 <ass91160@naver.com>
2025-11-12 16:42:47 +0900

    refactor(lotto): 구조체, 메서드, 트레이트 작성 순서 정렬

--- src/lotto/lotto.rs
+++ src/lotto/lotto.rs
@@ -2,58 +2,13 @@ use rand::Rng;
 use std::fmt::Display;
 
 pub struct Lotto {
-    pub lotto_numbers: Vec<i32>,
-}
-
-impl Display for Lotto {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let joined = self.join_by_delimiter();
-        write!(f, "[{}]", joined)
-    }
+    lotto_numbers: Vec<i32>,
 }
 
 impl Lotto {
-    fn join_by_delimiter(&self) -> String {
-        let joined = self
-            .lotto_numbers
-            .iter()
-            .map(|num| num.to_string())
-            .collect::<Vec<String>>()
-            .join(", ");
-
-        joined
-    }
-
-    fn generate_by_random() -> Lotto {
-        let mut rng = rand::thread_rng();
-        let mut vec: Vec<i32> = Vec::new();
-        while vec.len() != 6 {
-            let rand_number = rng.gen_range(1..=45);
-            if vec.contains(&rand_number) {
-                continue;
-            }
-            vec.push(rand_number);
-        }
-        vec.sort();
-        Lotto { lotto_numbers: vec }
-    }
-    pub fn contains(&self, number: &i32) -> bool {
-        self.lotto_numbers.contains(number)
-    }
-
-    pub fn get_count(&self, lotto: &Lotto) -> i32 {
-        let mut count = 0;
-        for target in &lotto.lotto_numbers {
-            if self.contains(target) {
-                count += 1;
-            }
-        }
-        count
-    }
-
     pub fn new(lotto_numbers: Vec<i32>) -> Result<Lotto, String> {
         if lotto_numbers.len() != 6 {
-            return Err("로또 번호는 6개여야 합니다.".to_string());
+            return Err("[ERROR] 로또 번호는 6개여야 합니다.".to_string());
         }
 
         for number in &lotto_numbers {
@@ -75,11 +30,57 @@ impl Lotto {
         })
     }
 
+    pub fn contains(&self, number: &i32) -> bool {
+        self.lotto_numbers.contains(number)
+    }
+
+    pub fn get_count(&self, lotto: &Lotto) -> i32 {
+        let mut count = 0;
+        for target in &lotto.lotto_numbers {
+            if self.contains(target) {
+                count += 1;
+            }
+        }
+        count
+    }
+
     pub fn generate_random_lottos(amount: i32) -> Vec<Lotto> {
         let mut lottos: Vec<Lotto> = Vec::new();
         while lottos.len() != amount.try_into().unwrap() {
             lottos.push(Lotto::generate_by_random());
         }
         lottos
+    }
+
+    fn generate_by_random() -> Lotto {
+        let mut rng = rand::thread_rng();
+        let mut vec: Vec<i32> = Vec::new();
+        while vec.len() != 6 {
+            let rand_number = rng.gen_range(1..=45);
+            if vec.contains(&rand_number) {
+                continue;
+            }
+            vec.push(rand_number);
+        }
+        vec.sort();
+        Lotto { lotto_numbers: vec }
+    }
+
+    fn join_by_delimiter(&self) -> String {
+        let joined = self
+            .lotto_numbers
+            .iter()
+            .map(|num| num.to_string())
+            .collect::<Vec<String>>()
+            .join(", ");
+
+        joined
+    }
+}
+
+impl Display for Lotto {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let joined = self.join_by_delimiter();
+        write!(f, "[{}]", joined)
     }
 }
